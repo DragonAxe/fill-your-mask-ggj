509e657c99bb02710976cd6e2cfabc787792ac45
Edge321 <pallares00@outlook.com>
2026-02-01 15:01:24 -0700

    Reformatted UI. Added new social battery

--- godot/src/components/c_social_battery.gd
+++ godot/src/components/c_social_battery.gd
@@ -2,16 +2,16 @@ class_name C_SocialBattery
 extends Component
 
 @export var change: float:
-    get:
-        return _change
-    set(value):
-        _change = value
+	get:
+		return _change
+	set(value):
+		_change = value
 
 @export var amount: float:
-    get:
-        return _amount
-    set(value):
-        _amount = clampf(value, 0.0, 100.0)
+	get:
+		return _amount
+	set(value):
+		_amount = clampf(value, 0.0, 100.0)
 
 @export var _change: float = 0.0
 @export var _amount: float = 100.0
--- godot/src/systems/s_update_social_battery_indicator.gd
+++ godot/src/systems/s_update_social_battery_indicator.gd
@@ -2,19 +2,19 @@ class_name UpdateSocialBatteryIndicatorSystem
 extends System
 
 func query():
-    return q.with_relationship([Relationship.new(C_Visualizes.new(), Player)]).with_all([C_SocialBatteryIndicator])
+	return q.with_relationship([Relationship.new(C_Visualizes.new(), Player)]).with_all([C_SocialBatteryIndicator])
 
 func process(entities: Array[Entity], _components: Array, _delta: float):
-    for indicator_entity in entities:
-        var comp = indicator_entity.get_component(C_SocialBatteryIndicator) as C_SocialBatteryIndicator
-        var rel = indicator_entity.get_relationship(Relationship.new(C_Visualizes.new()))
-        var ents_with_c_socialbattery = ECS.world.query.with_all([C_SocialBattery]).matches([rel.target])
-        
-        for player_entity in ents_with_c_socialbattery:
-            var c_battery = player_entity.get_component(C_SocialBattery) as C_SocialBattery
-            var battery_bar_node = indicator_entity.get_node(comp.battery_bar_path) as Range
-            var stress_bar_up_node = indicator_entity.get_node(comp.stress_bar_up_path) as Range
-            var stress_bar_down_node = indicator_entity.get_node(comp.stress_bar_down_path) as Range
-            battery_bar_node.value = c_battery.amount
-            stress_bar_up_node.value = lerpf(stress_bar_up_node.value, maxf(c_battery.change, 0.0) * 1000.0, 0.05)
-            stress_bar_down_node.value = lerpf(stress_bar_down_node.value, maxf(-c_battery.change, 0.0) * 1000.0, 0.05)
+	for indicator_entity in entities:
+		var comp = indicator_entity.get_component(C_SocialBatteryIndicator) as C_SocialBatteryIndicator
+		var rel = indicator_entity.get_relationship(Relationship.new(C_Visualizes.new()))
+		var ents_with_c_socialbattery = ECS.world.query.with_all([C_SocialBattery]).matches([rel.target])
+		
+		for player_entity in ents_with_c_socialbattery:
+			var c_battery = player_entity.get_component(C_SocialBattery) as C_SocialBattery
+			var battery_bar_node = indicator_entity.get_node(comp.battery_bar_path) as Range
+			var stress_bar_up_node = indicator_entity.get_node(comp.stress_bar_up_path) as Range
+			var stress_bar_down_node = indicator_entity.get_node(comp.stress_bar_down_path) as Range
+			battery_bar_node.value = c_battery.amount
+			stress_bar_up_node.value = lerpf(stress_bar_up_node.value, maxf(c_battery.change, 0.0) * 1000.0, 0.05)
+			stress_bar_down_node.value = lerpf(stress_bar_down_node.value, maxf(-c_battery.change, 0.0) * 1000.0, 0.05)
